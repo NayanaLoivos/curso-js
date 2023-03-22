95e6ba4aadbd9970689371710dceb157c7f52667
Nayana Loivos <loivosdev@gmail.com>
2023-03-22 13:18:52 -0300

    2: ways to run JS language

---
+++ basico/2-executar.js
@@ -0,0 +1,34 @@
+//Existem algumas maneiras de executar o código em JS
+
+//1. Por terminal do sistema operacional
+/*
+*digite node + o caminho absoluto do arquivo no seu computador, por exemplo:
+*node D:\Nayana\OneDrive\0-Pasta-estudo_Javascript\basico\2-executar.js 
+*/
+console.log('Executando JS por meio do node direto no terminal do sistema operacional!')
+
+//2. Por terminal integrado no editor
+/*
+*digite node + o caminho relativo do arquivo na pasta do projeto, por exemplo:
+*node basico\2-executar.js 
+*/
+console.log('Executando JS por meio do node no terminal do editor de código desde a pasta do projeto!')
+
+//2.1. Por terminal integrado no editor
+/*
+*digite node + o nome do arquivo na pasta específica que o arquivo estiver, por exemplo:    
+*2-executar.js 
+*/
+console.log('Executando JS por meio do node no terminal do editor de código desde a pasta que contém o arquivo!')
+
+//3. Por console no browser 
+/*
+*No chrome apertando a tecla F12.
+*/
+console.log('Executando JS diretamente no browser!')
+
+/*
+*V8 é o interpretador JavaScript, também chamado de máquina virtual Javascript (ou engine).
+*Foi desenvolvido pela Google.
+*É utilizado tanto no chrome quanto no backend pelo node.js.
+*/

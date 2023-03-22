64ee3cee8daacdb00725dca707c9f6485f7809fc
Nayana Loivos <loivosdev@gmail.com>
2023-03-22 10:56:02 -0300

    1: how to comment inside the code

---
+++ basico/1-comentarios.js
@@ -0,0 +1,16 @@
+/*Comentários nada mais é que uma anotação que é ignorada pelo 
+motor do JS e deve ter um propósito específico dentro do código.*/
+
+//1.Para explicar
+//Cometário de linha única
+console.debug('Comentários de linha única são ignorados pelo motor do JS.')
+/*
+Comentário de bloco
+*/
+console.debug('Comentários de bloco também são ignorados pelo motor do JS.')
+
+//2.Quando alguma linha no código não deve ser executada por algum propósito
+console.debug('Linha 1')
+//console.debug('Linha 2')
+console.debug('Linha 3')
+

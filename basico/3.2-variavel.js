50bf7370ae398f4c09866ed9666f23f3b169dcbf
Nayana Loivos <loivosdev@gmail.com>
2023-03-27 18:24:27 -0300

    3.2: purpose of variable

---
+++ basico/3.2-variavel.js
@@ -0,0 +1,22 @@
+//Usada com o propósito de salvar um valor na memória do computador em tempo de execução do código.
+/*
+    *Definimos variável por meio de palavras reservadas da linguagem: var, let ou const.
+
+    *Com a repetição da palavra 'eu' algumas vezes no texto abaixo.
+    *Através do recurso de criar variável podemos apontar 'eu' como um valor na memória,
+    *e quando precisamos do valor da variável, o invocamos pelo nome que definimos para ele.
+*/
+let pronome = 'eu';
+
+console.debug('"Quando', pronome,  'não sei onde guardei um papel importante e a procura revela-se inútil,')
+console.debug('pergunto-me: se', pronome, 'fosse', pronome, 'e tivesse um papel importante para guardar, que lugar escolheria?')
+console.debug('Às vezes dá certo. Mas muitas vezes fico tão pressionada pela frase "se', pronome, 'fosse', pronome, '",')
+console.debug('que a procura do papel se torna secundária, e começo a pensar, diria melhor SENTIR."')
+console.debug('---Clarice Lispector---')
+/*
+    *Sempre que o motor do JS observar o nome: pronome, ele buscará o seu valor atribuído.
+    *Já podemos notar a importância que uma variável tem na escrita de um código. 
+    *Toda vez que o valor da variável pronome precisar passar por alguma mudança,
+    *código, só será necessário mudá-lo apenas em uma única linha do código.
+*/
+

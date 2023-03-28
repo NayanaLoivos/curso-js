14b1ecb5e307554dc931f8dbe89d6729a452930b
Nayana Loivos <loivosdev@gmail.com>
2023-03-28 16:26:24 -0300

    3.3: the problem of declaring with var

---
+++ basico/3.3-diferenca-let-var.js
@@ -0,0 +1,11 @@
+//Diferenças relevantes entre criar variável com let ou com var
+
+// 1.Variável criada com var permite ser redeclarada
+var livro = 'Força de vontade não funciona';
+console.debug(livro);
+var livro = 'O homem que calculava';
+console.debug(livro);
+/*
+    *Isso é um PROBLEMA e não gera erro nenhum.
+*/
+

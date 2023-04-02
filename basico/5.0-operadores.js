c5b99bbaa7137d88e752cb4b766c5e13f66befab
Nayana Loivos <loivosdev@gmail.com>
2023-04-02 20:13:17 -0300

    5.0: operators in JS

---
+++ basico/5.0-operadores.js
@@ -0,0 +1,28 @@
+//Operadores gerais
+
+//Subtração(-)
+const anoNascimento = 1985;
+let anoAtual = 2023;
+console.log(`A minha idade em ${anoAtual} é ${anoAtual - anoNascimento} anos.`);
+
+//Adição ou concatenação(+)
+const num1 = 2;
+const num2 = 2;
+const str = '2';
+console.log(`O resultado da soma dos números é ${num1 + num2}.`);
+console.log(`O resultado da concatenação dos números é ${str + num2}.`);
+/*
+    *A concatenação vai depender do tipo de dado.
+*/
+
+//Multiplicação(*)
+console.log(`O resultado da multiplicação dos números é ${num1 * num2}.`);
+
+//Divisão(/)
+console.log(`O inteiro da divisão dos números é ${num1 / num2}.`);
+
+//Módulo(%)
+console.log(`O resto da divisão dos números é ${num1 % num2}.`);
+
+//Potência(**)
+console.log(`${num1} elevado a potência de ${num2} é ${num1 ** num2}.`);

171d72e64ff8075502276cffae491df4c0416d18
Nayana Loivos <loivosdev@gmail.com>
2023-04-07 10:59:03 -0300

    primitive data types and their immutability

--- basico/4.0-tipos-dados-primitivos.js
+++ basico/4.0-tipos-dados-primitivos.js
@@ -1,6 +1,8 @@
 //JS recebe uma tipagem dinâmica, isso diz que aos dados em JS são definidos de forma dinâmica.
 /*
-    *Os tipos de dados são: string, number, undefined, null, symbol
+    *Os tipos de dados primitivos são: string, number, undefined, null, boolean, bigint, symbol.
+    *Quando falando de tipos de dados primitivos, estamos falando de valores imutáveis, 
+    *ou seja, não conseguimos modificar nenhum elemento do dado fornecido.
 */
 
 //1.Tipo de dado undefined
@@ -21,6 +23,16 @@ let livro3 = `Código: a linguagem oculta de hardware e software de computador`;
 console.log( typeof(livro1)); // string
 console.log( typeof(livro2)); // string
 console.log( typeof(livro3)); // string
+
+livro1[0] = 'c'; 
+console.log(livro1); //continuará sem 'C' na posição[0] porque é imutável
+
+let letra1 =  'b';
+let letra2 = letra1;
+console.log(letra1, letra2);
+
+letra1 = 'c';
+console.log(letra1, letra2); //Tipos primitivos tem seus valores copiados
 
 //4.Tipo de dado number
 let numero1 = 10;

41e7062c389ab70f3529baab09c30de8c25d00e8
Nayana Loivos <loivosdev@gmail.com>
2023-04-19 21:14:52 -0300

    assignment operator

---
+++ basico/5.3-operador-atribuicao.js
@@ -0,0 +1,27 @@
+//Podemos nos expressar assim: 'contar += 1' ou assim: 'contar = contar + 1'
+
+//1. Somar
+const numero1 = 2;
+let contador1 = 0;
+contador1 += numero1;
+console.debug(contador1);
+contador1 += numero1;
+console.debug(contador1);
+
+//2. Subtrair
+const numero2 = 2;
+let contador2 = 11;
+contador2 -= numero2;
+console.debug(contador2);
+contador2 -= numero2;
+console.debug(contador2);
+
+//3. Multiplicar
+const numero3 = 2;
+let contador3 = 11;
+contador3 *= numero3;
+console.debug(contador3);
+contador3 *= numero3;
+console.debug(contador3);
+
+// Assim por diante com os operadores: **, /, %

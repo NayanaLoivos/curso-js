8010057a93918c9be1c1e6d9c433dcb696778874
Nayana Loivos <loivosdev@gmail.com>
2023-04-25 18:19:24 -0300

    value conversion

---
+++ basico/6.0-conversao-valor.js
@@ -0,0 +1,17 @@
+//É possível converter valores em JS
+
+const valor = parseInt('10.2');
+console.log(typeof valor);
+console.log(valor);
+const valor2 = parseFloat('10.2');
+console.log(typeof valor2);
+console.log(valor2);
+const valor3 = Number('10.2');
+console.log(typeof valor3);
+console.log(valor3);
+
+// PS: No momento de converter valores em JS é possível distinguir entre números inteiros, decimais ou deixar a critério do motor do JS
+
+
+
+

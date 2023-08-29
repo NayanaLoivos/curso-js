3d3fe810ea7cabd1a3dd4628f5d1fa24fd40f815
Nayana Loivos <loivosdev@gmail.com>
2023-08-29 14:56:56 -0300

    get values from inputs from text field component

--- curso-react/treino-projeto-organo/organo/src/components/TextField/TextField.js
+++ curso-react/treino-projeto-organo/organo/src/components/TextField/TextField.js
@@ -1,10 +1,17 @@
 import './TextField.css'
 
-function TextField(props) {    
+function TextField(props) {   
+    let valueInput = 'Nayana'
+
+    function onChange(event) {
+      valueInput = event.target.value
+      console.log(valueInput)
+    }
+    
     return(
         <div className="text-field">      
             <label>{props.label}</label>
-        <input required={props.mandatory} placeholder={props.placeholder} />
+        <input value={valueInput} onChange={onChange} required={props.mandatory} placeholder={props.placeholder} />
         </div>
     )
 }

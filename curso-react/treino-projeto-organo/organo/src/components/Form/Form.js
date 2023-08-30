717d1479ec0b5a93b97f5b4b69b91d68a0654670
Nayana Loivos <loivosdev@gmail.com>
2023-08-30 14:30:10 -0300

    delegated responsibility from TextField to Form

--- curso-react/treino-projeto-organo/organo/src/components/Form/Form.js
+++ curso-react/treino-projeto-organo/organo/src/components/Form/Form.js
@@ -2,6 +2,7 @@ import TextField from '../TextField/TextField.js';
 import DropdownList from '../DropdownList/DropdownList.js';
 import "./Form.css";
 import Button from '../Button/Button.js';
+import { useState } from 'react';
 
 
 function Form() {
@@ -15,19 +16,41 @@ function Form() {
     'Inovação e Gestão'
   ]
 
+  const [name, setNome] = useState('')
+  const [jobRole, setJobRole] = useState('') 
+  const [image, setImage] = useState('')
+
   function whenSaving(event) {
     event.preventDefault();
-    console.log('botão acionado, go event!')
+    console.log('Form foi submetido =>', name, jobRole, image)
+    console.log(event.target.value)
   }
 
   return (
     <section className="form">
       <form onSubmit={whenSaving}>
         <h2>Preencha os dados para criar o card</h2>
-        <TextField mandatory={true} label="Nome" placeholder="Digite seu nome" />
-        <TextField mandatory={true} label="Cargo" placeholder="Digite seu cargo" />
-        <TextField label="Imagem" placeholder="Digite o endereço da imagem" />
-        <DropdownList mandatory={true} label="Time" items={team} />
+        <TextField 
+          mandatory={true} 
+          label="Nome" 
+          placeholder="Digite seu nome"
+          valueInput={name}
+          changed={valueInput => setNome(valueInput)} />
+        <TextField 
+          mandatory={true} 
+          label="Cargo" 
+          placeholder="Digite seu cargo"
+          valueInput={jobRole}
+          changed={valueInput => setJobRole(valueInput)} />
+        <TextField 
+          label="Imagem" 
+          placeholder="Digite o endereço da imagem" 
+          valueInput={image}
+          changed={valueInput => setImage(valueInput)} />
+        <DropdownList 
+          mandatory={true} 
+          label="Time" 
+          items={team} />
         <Button>
           Criar Card
         </Button>
--- curso-react/treino-projeto-organo/organo/src/components/TextField/TextField.js
+++ curso-react/treino-projeto-organo/organo/src/components/TextField/TextField.js
@@ -1,18 +1,15 @@
 import { useState } from 'react'
 import './TextField.css'
 
-function TextField(props) {   
-    const [valueInput, setValue] = useState('Nayana')    
-    
-    function onChange(event) {
-        setValue(event.target.value)
-        console.log(valueInput)
+function TextField(props) {           
+    function onChange(event) {        
+        props.changed(event.target.value)       
     }
     
     return(
         <div className="text-field">      
             <label>{props.label}</label>
-        <input value={valueInput} onChange={onChange} required={props.mandatory} placeholder={props.placeholder} />
+        <input value={props.valueInput} onChange={onChange} required={props.mandatory} placeholder={props.placeholder} />
         </div>
     )
 }

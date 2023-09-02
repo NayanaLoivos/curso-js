cb7caf72c40235afbceadb897b6002912015e5e9
Nayana Loivos <loivosdev@gmail.com>
2023-09-01 21:01:51 -0300

    logical operators in JS

---
+++ basico/5.6-operadores-logicos.js
@@ -0,0 +1,16 @@
+/*
+    *Operadores lógicos:
+    && -> and 
+    || -> or
+    ! -> not
+*/
+
+console.log(true && true); //true
+console.log(true && false); //false
+
+console.log(true || true); //true
+console.log(true || false); //true
+console.log(false || false); //false
+
+console.log(!true); //false
+console.log(!false); //true

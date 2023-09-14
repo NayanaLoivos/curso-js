ba85675f59a1b998fab28aa5d12231b53619dac8
Nayana Loivos <loivosdev@gmail.com>
2023-09-13 21:48:12 -0300

    objects in the browser

---
+++ basico/html-JS/1-objetos-basicos-browser.js
@@ -0,0 +1,18 @@
+//Objetos do navegador (front-end)
+
+//Tipo primitivo undefined
+alert('Mensagem ok!'); //Está dentro do objeto window.
+window.alert('Mensagem ok!');
+/*
+    *Podemos observar no browser que o tipo é undefined porque não aponta para lugar nenhum na memória.
+    *window - é um objeto que refere-se a janela completa do browser.
+*/
+
+//Tipo primitivo boolean
+confirm('Quer cancelar?'); //ok-true ou cancela-false
+
+//Tipo primitivo string
+prompt('Você entende o conteúdo até aqui? [s / n]');
+
+
+
